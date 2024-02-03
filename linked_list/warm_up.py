a7d699bf5b352152c2d6eea3c4691ef0b1765871
Ayush <daveayush51@gmail.com>
2024-02-03 21:25:43 +1100

    new changes

--- linked_list/warm_up.py
+++ linked_list/warm_up.py
@@ -110,3 +110,57 @@ c.next = d
 ## get_node_value(a, 2) # 'c'
 
 
+# def reverse_list(head):             # Reverse the list
+#     prev = None
+#     current = head
+#     while current != None:
+#         current.next = prev
+#         next = current.next
+#         prev = current
+#         current = next
+#     return prev
+
+
+# def reverse_list(head, prev=None):      # Recursion
+#     if head == None: return prev
+#     next = head.next
+#     head.next = prev
+#     reverse_list(next, head)
+
+# reverse_list(a)
+
+
+# def zipperlist(head1, head2):
+#     tail = head1
+#     current1 = head1.next
+#     current2 = head2
+#     count = 0
+
+#     while (current1 != None and current2 != None):
+#         if count % 2 == 0:
+#             tail.next = current2
+#             current2 = current2.next
+#         else:
+#             tail.next = current1
+#             current2 = current1.next
+#         tail = tail.next
+#         count += 1
+
+#         if current1 != None: tail.next = current1
+#         if current2 != None: tail.next = current2
+
+#     return head1
+
+
+# def zipperlist(head1, head2):
+#     if (head1 == None and head2 == None): return None
+
+#     if head1 == None: return head2
+#     if head2 == None: return head1
+
+#     next1 = head1.next
+#     next2 = head2.next
+#     head1.next = head2
+#     head2.next = zipperlist(next1, next2)
+
+#     return head1
